08d5bd5d0b0254b176002bebf4240e3b15c89189
Christophe <christophe@tralalere.com>
2017-03-21 14:17:48 +0100

    Update package.json

--- examples/sprites/sprites.ts
+++ examples/sprites/sprites.ts
@@ -1,4 +1,4 @@
-import {Sprite, File} from "blip-framework/compiled/core";
+import {Sprite, File} from "blip-framework/core";
 
 var file:File = new File("files/sprites/p4-body.png");
 var sprite:Sprite = new Sprite(file, 20, 20);
