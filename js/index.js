c5da73c1129255a1ee1605f1f30bec5c56ac07c7
Christophe Vimbert <c.vimbert@outlook.fr>
2017-04-04 08:13:12 +0200

    Dev

--- js/index.js
+++ js/index.js
@@ -6,14 +6,6 @@
 
     var dictionary = ["Sprite", "File"];
 
-    var pagesPaths = [
-        "intro",
-        "mise-en-place",
-        "api/sprites",
-        "api/groups",
-        "api/group-states"
-    ];
-
     var tableOfContent = {
         "blip-project": {
             title: "Le projet Blip",
@@ -57,13 +49,14 @@
             var pageContents = {};
             var terUls = {};
 
+            // todo: attention, ici l'ordre n'est pas bon !!!
             for (var pageId in datas) {
                 if (datas.hasOwnProperty(pageId)) {
                     var pageContent = $("<div></div>");
                     pageContent.html($(datas[pageId]));
 
                     var title = $("h1", pageContent).html();
-                    var subLi = $("<li></li>");
+                    var subLi = $("<li class='level-2'></li>");
                     subLi.data("pid", pageId);
                     subLi.html(title);
 
@@ -94,163 +87,30 @@
 
         var sections = $("section", pageContent);
 
-        sections.each(function () {
+        sections.each(function (i) {
+
+            $(this).attr("id", "section-" + i);
+
             var sectionTitle = $("h2", this).html();
 
-            var subSubLi = $("<li></li>");
+            var subSubLi = $("<li class='level-3'></li>");
             subSubLi.html(sectionTitle);
 
             subSubLi.on("click", function () {
-
+                document.location.href = "#section-" + i;
+                displayPage(pageContent);
             });
 
             containerUl.append(subSubLi);
         });
+
+        displayPage(pageContent);
     }
 
-    function loadPages(pagesIndex, callback) {
-
-        var pagesHtml = {};
-        var count = 0;
-
-        var index2Container = $("#index2");
-        var subs = {};
-
-        if (!qParam) {
-            qParam = Object.keys(tableOfContent)[0];
-            pushLocaleUrl();
-        }
-
-        for (var id in tableOfContent) {
-
-            if (tableOfContent.hasOwnProperty(id)) {
-                var contentSet = tableOfContent[id];
-
-                var li = $("<li class='level-1'></li>");
-                li.attr("id", "lv1-" + id);
-                li.html(contentSet["title"]);
-                li.data("lid", id);
-                index2Container.append(li);
-
-                li.on("click", function () {
-
-                    // pour un effet ulterieur (joli effet de repliage)
-                    //alert (subs[qParam].height());
-
-                    var qp = $(this).data("lid");
-                    qParam = qp;
-                    rParam = null;
-                    pushLocaleUrl();
-
-                    $(".level-1-sub").html("");
-
-                    generateSubMenu(qp, tableOfContent[qp], subs);
-                });
-
-                var sub = $("<ul class='level-1-sub'></ul>");
-                sub.attr("id", id + "-sub");
-                index2Container.append(sub);
-
-                subs[id] = sub;
-
-                if (qParam === id) {
-                    generateSubMenu(id, contentSet, subs);
-                }
-            }
-        }
-
-        for (var i = 0; i < pagesIndex.length; i++) {
-            loadPage(pagesIndex[i], function (path, html) {
-                pagesHtml[path] = html;
-                count++;
-
-                if (count === pagesIndex.length) {
-                    callback(pagesHtml);
-                }
-            });
-        }
-    }
-    
-    function createPagesSetDisplay(contentSet) {
-        
-    }
-
-    String.prototype.replaceAll = function (search, replacement) {
-        var tmp = this;
-
-        while (tmp.indexOf(search) !== -1) {
-            tmp = tmp.replace(search, replacement);
-        }
-
-        return tmp;
-    };
-
-    Array.prototype.each = function (callback) {
-        for (var i = 0; i < this.length; i++) {
-            callback.call(this[i]);
-        }
-    };
-
-    // term, actionType, actionArg
-    String.prototype.applyDictionary = function (dictionary) {
-        var tmp = this;
-
-        for (var i = 0; i < dictionary.length; i++) {
-            tmp = tmp.replaceAll(dictionary[i], "<span class='dictionary-entry " + dictionary[i].toLowerCase() + "-entry'>TMP</span>");
-            tmp = tmp.replaceAll("TMP", dictionary[i]);
-        }
-
-        return tmp;
-    };
-
-    function loadPage(pagePath, callback) {
-
-        var req = new XMLHttpRequest();
-        req.overrideMimeType("text/html");
-        req.open('GET', "pages/" + pagePath + ".html");
-        req.onload = function () {
-            callback(pagePath, req.responseText);
-        };
-        req.send();
-    }
-
-    function loadPageB(directory, pagePath, callback) {
-
-        var req = new XMLHttpRequest();
-        req.overrideMimeType("text/html");
-        req.open('GET', directory + "/" + pagePath + ".html");
-        req.onload = function () {
-            callback(pagePath, req.responseText);
-        };
-        req.send();
-    }
-
-    function loadPagesSet(id, pagesPaths, callback) {
-
-        var pagesData = {};
-        var count = 0;
-
-        for (var i = 0; i < pagesPaths.length; i++) {
-
-            loadPageB("pages/" + id, pagesPaths[i], function (path, pageContent) {
-                pagesData[path] = pageContent;
-                count++;
-
-                if (count === pagesPaths.length) {
-                    callback(pagesData, id);
-                }
-            });
-        }
-    }
-
-    function contructPage(pageId, pageHtml) {
-
-        $(".sub-menu").html("");
-
-        //document.body.style.visibility = "visible";
+    function displayPage(pageElem) {
 
         var htmlContainer = $("#right");
-        var contentHtml = $(pageHtml);
+        var contentHtml = pageElem;
         htmlContainer.html(contentHtml);
 
         var sections = $("section");
@@ -258,22 +118,6 @@
         var id = 0;
 
         sections.each(function () {
-            var title = $("h2", this);
-            var idt = "t" + id;
-            title.attr("id", idt);
-
-            var subElem = $(
-                "<li>" +
-                    "<a></a>" +
-                "</li>"
-            );
-
-            $("#sub-menu-" + cleanPath(pageId)).append(subElem);
-
-            var link = $("a", subElem);
-            link.html(title.html());
-            link.attr("href", "#" + idt);
-            id++;
 
             // et les liens
             var links = $("a", this);
@@ -336,6 +180,115 @@
         });
     }
 
+    function loadPages(callback) {
+
+        var pagesHtml = {};
+        var count = 0;
+
+        var index2Container = $("#index");
+        var subs = {};
+
+        if (!qParam) {
+            qParam = Object.keys(tableOfContent)[0];
+            pushLocaleUrl();
+        }
+
+        for (var id in tableOfContent) {
+
+            if (tableOfContent.hasOwnProperty(id)) {
+                var contentSet = tableOfContent[id];
+
+                var li = $("<li class='level-1'></li>");
+                li.attr("id", "lv1-" + id);
+                li.html(contentSet["title"]);
+                li.data("lid", id);
+                index2Container.append(li);
+
+                li.on("click", function () {
+
+                    // pour un effet ulterieur (joli effet de repliage)
+                    //alert (subs[qParam].height());
+
+                    var qp = $(this).data("lid");
+                    qParam = qp;
+                    rParam = null;
+                    pushLocaleUrl();
+
+                    $(".level-1-sub").html("");
+
+                    generateSubMenu(qp, tableOfContent[qp], subs);
+                });
+
+                var sub = $("<ul class='level-1-sub'></ul>");
+                sub.attr("id", id + "-sub");
+                index2Container.append(sub);
+
+                subs[id] = sub;
+
+                if (qParam === id) {
+                    generateSubMenu(id, contentSet, subs);
+                }
+            }
+        }
+    }
+
+    String.prototype.replaceAll = function (search, replacement) {
+        var tmp = this;
+
+        while (tmp.indexOf(search) !== -1) {
+            tmp = tmp.replace(search, replacement);
+        }
+
+        return tmp;
+    };
+
+    Array.prototype.each = function (callback) {
+        for (var i = 0; i < this.length; i++) {
+            callback.call(this[i]);
+        }
+    };
+
+    // term, actionType, actionArg
+    String.prototype.applyDictionary = function (dictionary) {
+        var tmp = this;
+
+        for (var i = 0; i < dictionary.length; i++) {
+            tmp = tmp.replaceAll(dictionary[i], "<span class='dictionary-entry " + dictionary[i].toLowerCase() + "-entry'>TMP</span>");
+            tmp = tmp.replaceAll("TMP", dictionary[i]);
+        }
+
+        return tmp;
+    };
+
+    function loadPage(directory, pagePath, callback) {
+
+        var req = new XMLHttpRequest();
+        req.overrideMimeType("text/html");
+        req.open('GET', directory + "/" + pagePath + ".html");
+        req.onload = function () {
+            callback(pagePath, req.responseText);
+        };
+        req.send();
+    }
+
+    function loadPagesSet(id, pagesPaths, callback) {
+
+        var pagesData = {};
+        var count = 0;
+
+        for (var i = 0; i < pagesPaths.length; i++) {
+
+            loadPage("pages/" + id, pagesPaths[i], function (path, pageContent) {
+                pagesData[path] = pageContent;
+                count++;
+
+                if (count === pagesPaths.length) {
+                    callback(pagesData, id);
+                }
+            });
+        }
+    }
+
     function cleanPath(path) {
         return path.replace("/", "-");
     }
@@ -395,46 +348,8 @@
         System.load(currentScriptId).catch(function(err){ console.error(err); });
     }
 
-    loadPages(pagesPaths, function (pages) {
-
-        var index = $("#index");
-        var currentPageId = getId("q");
-
-        for (var i = 0; i < pagesPaths.length; i++) {
-
-            var element = $("<div></div>");
-
-            var contentElement = $(pages[pagesPaths[i]]);
-            element.append(contentElement);
-
-            var title = $("h1", element).html();
-
-            var li = $("<li class='menu-item'></li>");
-            var sub = $("<ul class='sub-menu'></ul>");
-            sub.attr("id", "sub-menu-" + cleanPath(pagesPaths[i]));
-
-            li.attr("id", cleanPath(pagesPaths[i]));
-
-            li.attr("data-id", i);
-            //al.attr("href", "?q=" + cleanPath(pagesPaths[i]));
-
-            li.on("click", function () {
-                currentPageId = pagesPaths[$(this).data("id")];
-                contructPage(currentPageId, pages[currentPageId]);
-            });
-
-            li.html(title);
-
-            index.append(li);
-            index.append(sub);
-
-            if (cleanPath(pagesPaths[i]) === currentPageId) {
-
-                contructPage(currentPageId, pages[pagesPaths[i]]);
-            }
-        }
-
-
+    loadPages(function (pages) {
+        // plus rien !!!
     });
 
 })();
